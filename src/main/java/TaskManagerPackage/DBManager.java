6504e84949739eb6ec3e40639294affc14ea5cf7
Caleb Buckley <trt2862@autuni.ac.nz>
2024-05-17 20:05:21 +1200

    Created DBManager Class - Template no implementation yet.

---
+++ src/main/java/TaskManagerPackage/DBManager.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TaskManagerPackage;
+
+/**
+ *
+ * @author Caleb
+ */
+public class DBManager {
+
+    //to handle DB actions
+    //to replace FileManager and tasks.txt
+    public DBManager() {
+
+    }
+
+    public void add() {
+        //add task to DB.
+
+    }
+
+    public void update() {
+        //update existing task in DB
+    }
+
+    public boolean exists() {
+        //checks if task exists in DB.
+        return false;
+    }
+
+    public void search() {
+        //returns task being searched for.
+    }
+
+    public void remove() {
+        //remove task from DB..
+    }
+
+    public void undo() {
+        //undo previous operation.
+    }
+
+    public void sort() {
+        //sort list based on a comparator.
+    }
+}
